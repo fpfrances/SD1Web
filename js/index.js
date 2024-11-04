3b4be38d58dc4448bfcf379e4b5cef23e6ed5add
Filipe <89738919+fpfrances@users.noreply.github.com>
2024-11-04 12:58:11 -0500

    Created conference page and modified reports page

--- js/index.js
+++ js/index.js
@@ -6,3 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+function openPDF(pdfUrl) {
+    const pdfViewer = document.getElementById('pdf-viewer');
+
+    // Check if the viewer is already open with the same document
+    if (pdfViewer.dataset.currentPdf === pdfUrl) {
+        pdfViewer.style.height = "0";
+        pdfViewer.dataset.currentPdf = "";  // Reset PDF data
+        pdfViewer.innerHTML = "";  // Clear PDF
+    } else {
+        pdfViewer.innerHTML = `<iframe src="${pdfUrl}" width="100%" height="100%"></iframe>`;
+        pdfViewer.style.height = "1130px";  // Expand to show the PDF
+        pdfViewer.dataset.currentPdf = pdfUrl;  // Store the current PDF
+    }
+}
